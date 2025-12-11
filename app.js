0239ac8ae1198649a85e96003c8d12be8616fa24
JovialDuplex <takeuhduplex2006@gmail.com>
2025-12-11 20:09:06 +0100

    Legere modification pour le push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (request, response)=> {
-    response.send("Welcome to My Application !");
+    response.send("Welcome to My Application, the server is running on port : " + port);
 });
 
 app.listen(port, ()=>{
